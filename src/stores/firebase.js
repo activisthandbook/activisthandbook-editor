283c1df1f31b879978270b552141d089913ad7e5
Joppe Hoekstra <joppehoekstra@icloud.com>
2022-12-29 12:46:41 +0100

    Add app check

--- src/stores/firebase.js
+++ src/stores/firebase.js
@@ -18,6 +18,7 @@ import { getStorage } from "firebase/storage";
 import { getDatabase } from "firebase/database";
 
 import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
+
 import { getPerformance } from "firebase/performance";
 
 import { Notify } from "quasar";
@@ -127,6 +128,18 @@ export const useFirebaseStore = defineStore("firebase", {
       //   /* Optional argument. If true, the SDK automatically refreshes App Check tokens as needed. */
       //   isTokenAutoRefreshEnabled: true,
       // });
+
+      // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+      // key is the counterpart to the secret key you set in the Firebase console.
+      const appCheck = initializeAppCheck(this.firebaseApp, {
+        provider: new ReCaptchaV3Provider(
+          "6LfbkrcjAAAAADRXu0PSoqJuJPR5f0XmkU5PUSjW"
+        ),
+
+        // Optional argument. If true, the SDK automatically refreshes App Check
+        // tokens as needed.
+        isTokenAutoRefreshEnabled: true,
+      });
 
       /* 🤖 PERFORNANCE MONITORING
        */
