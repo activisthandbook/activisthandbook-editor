bd3b1137f81c848891651a5a18ed3bc911ede263
Joppe Hoekstra <joppehoekstra@icloud.com>
2023-01-08 16:58:53 +0100

    typo

--- src/pages/StartPage.vue
+++ src/pages/StartPage.vue
@@ -217,7 +217,7 @@ export default {
         this.loading = true;
         await this.usersStore.saveUser(
           this.firebaseStore.auth.currentUser.uid,
-          firebaseStore.auth.currentUser.uid
+          this.firebaseStore.auth.currentUser.uid
         );
         this.loading = false;
         this.$q.notify({
